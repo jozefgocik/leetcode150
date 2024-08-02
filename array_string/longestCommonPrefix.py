936b33cc75ddd3442e179e9b2ff39643e7dd51e0
jozefgocik <jozefgocik03@gmail.com>
2024-08-02 09:10:05 +0200

    Created longestCommonPrefix

---
+++ array_string/longestCommonPrefix.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/longest-common-prefix/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        return
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.longestCommonPrefix(strs=["flower", "flow", "flight"]))
+    print(solution.longestCommonPrefix(strs=["dog", "racecar", "car"]))

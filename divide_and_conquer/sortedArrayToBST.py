f7b68e8ef2b185dc47300eb717ba399595aa7d76
jozefgocik <jozefgocik03@gmail.com>
2024-12-18 09:14:57 +0100

    Added sortedArrayToBST

---
+++ divide_and_conquer/sortedArrayToBST.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/?envType=study-plan-v2&envId=top-interview-150
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+
+    # Solution 1
+    # O(n) time / O(n) space
+    def sortedArrayToBST(self, nums):
+        if not nums:
+            return
+
+        index = len(nums)//2
+        root = TreeNode(nums[index])
+
+        root.left = self.sortedArrayToBST(nums[0:index])
+        root.right = self.sortedArrayToBST(nums[index+1:])
+
+        return root
